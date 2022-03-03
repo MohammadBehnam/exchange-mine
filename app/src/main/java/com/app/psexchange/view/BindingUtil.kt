73a9dd97333981aefb498e618cfd701e33138a0d
Mohammad Aghili <mohammad.aghili@outlook.com>
2022-03-03 17:01:18 +0330

    - Hot fixes

--- app/src/main/java/com/app/psexchange/view/BindingUtil.kt
+++ app/src/main/java/com/app/psexchange/view/BindingUtil.kt
@@ -2,6 +2,7 @@ package com.app.psexchange.view
 
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import com.app.psexchange.model.Balance
 import java.text.DecimalFormat
 
 object BindingUtil {
@@ -14,5 +15,37 @@ object BindingUtil {
       val text = "+" + DecimalFormat("##.##").format(value)
       view.text = text
     }
+  }
+  
+  @JvmStatic
+  @BindingAdapter("setBalance")
+  fun setBalance(view: TextView, balance: Balance?) {
+    val currency = balance?.currency
+    val value = getBalanceValueString(balance = balance)
+    val text = "$value $currency"
+    view.text = text
+  }
+  
+  @JvmStatic
+  @BindingAdapter("setBalanceError")
+  fun setBalanceError(view: TextView, balance: Balance?) {
+    val currency = balance?.currency
+    val value = getBalanceValueString(balance = balance)
+    val text = "<= $value $currency"
+    view.text = text
+  }
+  
+  private fun getBalanceValueString(balance: Balance?): String{
+    if (balance != null){
+      val arr = balance.value.toString().split(".")
+      if (arr.size > 1){
+        return if (arr[1].length < 2){
+          arr[0] + "." + arr[1] + "0"
+        } else {
+          balance.value.toString()
+        }
+      }
+    }
+    return "0.00"
   }
 }
